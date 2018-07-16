43a12967b19c67046aa7940783786a9ba7827cf3
ilovebella02 <kirthipar@gmail.com>
2018-07-16 11:53:20 -0700

    Add Stacks.py

---
+++ Advanced Data Structure/Stacks.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+
+class My_Stack():
+    def __init__(self):
+        self._stack = []
+        
+    def _repr_(self):
+        return" ",join(str(x) for x in self._stack.reverse())
+        
+    def push(self, item):
+        self._stack.append(item)
+        
+    def pop(self):
+        return self._stack.pop()
+    
+    def peek(self):
+        return self._stack[-1]
+    
+stack = My_Stack()
+stack.push("Red")
+stack.push("Green")
+stack.push("Blue")
+
+stack.peek() == "Blue"
+stack.pop() == "Blue"
+print(stack)
+        
